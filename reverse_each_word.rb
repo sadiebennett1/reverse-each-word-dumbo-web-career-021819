dc583fc30f1499be26accb8cfae5d601bc41980e
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 05:02:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
   new_wordarr = str.split(" ")
   new_wordstr = ""
-  new_word.each do |word|
+  new_wordarr.each do |word|
     new_wordstr += word.reverse + " "
   end
   return new_wordstr
